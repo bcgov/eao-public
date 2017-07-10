09344d7c967bc4ca1cf31a03f2fde307f8d2609c
Mark Lise <mark.lise@gmail.com>
2017-07-10 14:57:18 -0700

    Adding invalid property

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent implements OnInit {
+  invalid: boolean;
 
   constructor() { }
 
