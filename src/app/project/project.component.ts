62db9abc46ed2723a07dc21ebdef3e48c2a1b761
Mark Lisé <marklise@users.noreply.github.com>
2017-10-04 20:20:49 -0700

    Revert "EPIC-1244 EAO Public: fixed crash when accessing proponents."

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -65,11 +65,11 @@ export class ProjectComponent implements OnInit {
     for (let i = 0; i < data.length; i++) {
       if (!data[i].proponent) {
         // In case a proponent object isn't set yet.
-        data[i].proponent = { name: '' };
+        data[i].proponent = {name: null};
       }
-      if (data[i].proponent.name && this.proponents.indexOf(data[i].proponent.name) === -1) {
-        this.proponents.push({ name: data[i].proponent.name });
-      }
+      (data[i].proponent.name && this.proponents.indexOf(data[i].proponent.name) === -1)
+        ? this.proponents.push({name: data[i].proponent.name})
+        : console.log('This item already exists');
     }
   }
   findWithAttr(array, attr, value) {
