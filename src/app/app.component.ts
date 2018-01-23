0c3e1f8b8d9404e343bde92aa9bb1d573c7a870b
Zhou-Charles <BZHOUCHA@BCGOV>
2018-01-23 09:12:43 -0800

    EM-147 added check for if cookie exists and creates one if it doesn't

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,9 +44,15 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.loggedIn = this.cookieService.get('loggedIn');
+    if (this.loggedIn) {
+      this.cookieService.set('loggedIn', 'false');
+    } else {
+      this.cookieService.set('loggedIn', 'false');
+      this.loggedIn = this.cookieService.get('loggedIn');
+    }
     this._router.events.subscribe((url: any) => {
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
     });
-  }
+  };
 }
