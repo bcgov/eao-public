1062a5c983fce76d60c5c36a1d2e61003455023f
Zhou-Charles <BZHOUCHA@BCGOV>
2018-01-26 08:39:15 -0800

    removed unnecessary code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -44,12 +44,11 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.loggedIn = this.cookieService.get('loggedIn');
-    if (this.loggedIn) {
-      this.cookieService.set('loggedIn', 'false');
-    } else {
+    if (!this.loggedIn) {
       this.cookieService.set('loggedIn', 'false');
       this.loggedIn = this.cookieService.get('loggedIn');
     }
+
     this._router.events.subscribe((url: any) => {
       document.body.scrollTop = 0;
       document.documentElement.scrollTop = 0;
