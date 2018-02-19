e0c7c6025431c9e7802d70bca4dd47327b2d5328
Zhou-Charles <BZHOUCHA@BCGOV>
2018-02-19 09:31:00 -0800

    em-327 added attributes to model

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -12,6 +12,11 @@ export class Project {
   openCommentPeriod: string;
   eacDecision: string;
   currentPhase: CurrentPhase;
+  region: string;
+  location: string;
+  projectLead: string;
+  responsibleEPD: string;
+  CELead: string;
   constructor(obj?: any) {
     this._id = obj && obj._id || null;
     this.code = obj && obj.code || null;
@@ -23,5 +28,10 @@ export class Project {
     this.openCommentPeriod = obj && obj.openCommentPeriod || null;
     this.eacDecision = obj && obj.eacDecision || null;
     this.currentPhase = obj && obj.currentPhase || null;
+    this.region = obj && obj.region || null;
+    this.location = obj && obj.location || null;
+    this.projectLead = obj && obj.projectLead || null;
+    this.responsibleEPD = obj && obj.responsibleEPD || null;
+    this.CELead = obj && obj.CELead || null;
   }
 }
