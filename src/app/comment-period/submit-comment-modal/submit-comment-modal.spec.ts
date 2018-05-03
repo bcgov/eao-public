beb5d56a3f9e79622b39474409b121cb9fb5342b
Nick Phura <Nick.Phura@quartech.com>
2018-05-02 17:02:46 -0700

    Fix rebase issues: add missing unit tests, remove trivial map tests.

--- src/app/comment-period/submit-comment-modal/submit-comment-modal.spec.ts
+++ src/app/comment-period/submit-comment-modal/submit-comment-modal.spec.ts
@@ -1,8 +1,11 @@
 import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { Http, HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { NgxPaginationModule } from 'ngx-pagination';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 import { CommentPeriodComponent } from '../comment-period.component';
 import { SubmitCommentModalComponent } from './submit-comment-modal';
@@ -11,89 +14,153 @@ import { SubmitCommentProgressModalComponent } from './../submit-comment-progres
 
 import { OrderByPipe } from '../../order-by.pipe';
 import { ObjectFilterPipe } from '../../object-filter.pipe';
-
-import { Api } from '../../services/api';
+import { CommentPeriod } from '../../models/commentperiod';
+import { Comment } from '../../models/comment';
+import { Document } from '../../models/document';
+import { ValuedComponent } from '../../models/vcs';
+import { Project } from '../../models/project';
 import { CommentPeriodService } from '../../services/comment-period.service';
 
-describe('SubmitCommentModalComponent', () => {
-  let component: SubmitCommentModalComponent;
-  let fixture: ComponentFixture<SubmitCommentModalComponent>;
+describe('CommentPeriodComponent', () => {
+  let component: CommentPeriodComponent;
+  let fixture: ComponentFixture<CommentPeriodComponent>;
+  let ActivatedRouteStub;
 
   beforeEach(async(() => {
     // stub activated route
+    ActivatedRouteStub = {
+      snapshot: { params: { id: 1, code: 2 } }
+    };
     TestBed.configureTestingModule({
       providers: [
-        Api,
-        CommentPeriodService,
-        CommentPeriodComponent
+        { provide: ActivatedRoute, useValue: ActivatedRouteStub },
+        {
+          provide: CommentPeriodService,
+          useValue: jasmine.createSpyObj('CommentPeriodService', [
+            'getByCode',
+            'getCommentsAndDocuments'
+          ])
+        }
       ],
       imports: [
         FormsModule,
+        NgxPaginationModule,
         HttpModule,
-        RouterTestingModule,
-        NgxPaginationModule
+        RouterTestingModule
       ],
       declarations: [
-        SubmitCommentModalComponent,
+        CommentPeriodComponent,
         PCPInfoModalComponent,
+        SubmitCommentModalComponent,
         SubmitCommentProgressModalComponent,
         OrderByPipe,
         ObjectFilterPipe
       ]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
+
   beforeEach(() => {
-    fixture = TestBed.createComponent(SubmitCommentModalComponent);
+    fixture = TestBed.createComponent(CommentPeriodComponent);
     component = fixture.componentInstance;
+
+    const commentPeriodService = TestBed.get(CommentPeriodService);
+    commentPeriodService.getByCode.and.returnValue(
+      Observable.of(new CommentPeriod())
+    );
+    commentPeriodService.getCommentsAndDocuments.and.returnValue(
+      Observable.of(
+        new CommentPeriod({
+          comments: <Comment[]>[
+            new Comment({
+              comment: 'someComment',
+              documents: ['someDocument'],
+              vcs: <ValuedComponent[]>[new ValuedComponent()]
+            })
+          ],
+          openHouses: ['openHouses'],
+          project: new Project({ name: 'someProject' })
+        })
+      )
+    );
+
     fixture.detectChanges();
   });
   describe('ngOnInit()', () => {
-    it('should create a comment object', () => {
-      expect(component.comment).toBeTruthy();
+    it('should return data for route.snapshot.params.id', () => {
+      const activatedRoute = TestBed.get(ActivatedRoute);
+      expect(activatedRoute.snapshot.params.id).toEqual(1);
+    });
+    it('should return data for route.snapshot.params.code', () => {
+      const activatedRoute = TestBed.get(ActivatedRoute);
+      expect(activatedRoute.snapshot.params.code).toEqual(2);
+    });
+    it('should return data for commentPeriod', () => {
+      expect(component.commentPeriod).toBeTruthy;
+      expect(component.commentPeriod.comments[0].comment).toEqual('someComment');
+    });
+    it('should set column to dateAdded', () => {
+      expect(component.column).toBe('dateAdded');
+    });
+    it('should set direction to -1', () => {
+      expect(component.direction).toBe(-1);
+    });
+    it('should set loading to false', () => {
+      expect(component.loading).toBe(false);
     });
   });
-  describe('validateFields(form)', () => {
-    it('should set valid to false when author is empty', () => {
-      const form = {
-        author: '',
-        location: '',
-        comment: ''
-      };
-      component.validateFields(form);
-
-      expect(component.valid).toBe(false);
+  describe('comment readmore property', () => {
+    let commentKeys;
+    describe('on load', () => {
+      it('should initially be undefined', () => {
+        commentKeys = Object.keys(component.commentPeriod.comments[0]);
+        expect(commentKeys.includes('readmore')).toBeFalsy;
+      });
     });
-    it('should set valid to false when location is empty', () => {
-      const form = {
-        author: 'aa',
-        location: '',
-        comment: ''
-      };
-      component.validateFields(form);
-
-      expect(component.valid).toBe(false);
+    describe('after expanding a comment', () => {
+      it('should be defined', () => {
+        component.readmore(component.commentPeriod.comments[0]);
+        commentKeys = Object.keys(component.commentPeriod.comments[0]);
+        expect(commentKeys.includes('readmore')).toBeTruthy;
+      });
     });
-    it('should set valid to false when comment is empty', () => {
-      const form = {
-        author: 'aa',
-        location: 'aa',
-        comment: ''
-      };
-      component.validateFields(form);
+  });
+  describe('sort(property)', () => {
+    let property;
 
-      expect(component.valid).toBe(false);
+    beforeEach(() => {
+      property = 'dateAdded';
     });
-    it('should set valid to true when all validation passes', () => {
-      const form = {
-        author: 'aa',
-        location: 'aa',
-        comment: 'a'
-      };
-      component.valid = true;
-      component.validateFields(form);
+    describe('given isDesc is true', () => {
+      beforeEach(() => {
+        component.isDesc = true;
+        component.sort(property);
+      });
 
-      expect(component.valid).toBe(true);
+      it('should set isDesc to false', () => {
+        expect(component.isDesc).toBeFalsy();
+      });
+      it('should set direction to -1', () => {
+        expect(component.direction).toBe(-1);
+      });
+    });
+    describe('given isDesc is false', () => {
+      beforeEach(() => {
+        component.isDesc = false;
+        component.sort(property);
+      });
+
+      it('should set isDesc to true', () => {
+        expect(component.isDesc).toBeTruthy();
+      });
+      it('should set direction to 1', () => {
+        expect(component.direction).toBe(1);
+      });
+    });
+    describe('given property', () => {
+      it('should assign property to column', () => {
+        component.sort(property);
+        expect(component.column).toBe(property);
+      });
     });
   });
 });
