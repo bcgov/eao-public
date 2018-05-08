3ac23a15a3b6b2d0fd370767c39402979507cfe1
Emiliano Suñé <emiliano.sune@quartech.com>
2018-05-08 11:28:58 -0700

    EM-909: Display related document size in public comment period

--- src/app/models/document.ts
+++ src/app/models/document.ts
@@ -1,12 +1,19 @@
+
+import * as filesize from 'filesize';
 export class Document {
   _id: number;
   isPublished: boolean;
   documentFileURL: string;
   displayName: string;
+  internalSize: number;
+  fileSize: string;
   constructor(obj?: any) {
     this._id = obj && obj._id || null;
     this.isPublished = obj && obj.isPublished || false;
     this.documentFileURL = obj && obj.documentFileURL || null;
     this.displayName = obj && obj.displayName || null;
+    this.internalSize = obj && obj.internalSize || null;
+    // TODO: use a proper filter such as https://github.com/amitdahan/ngx-filesize when upgrading angular
+    this.fileSize = obj && filesize(obj.internalSize) || null;
   }
 }
