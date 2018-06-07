50252f1e263abac0c7fa00fb3aab4ae3f929c48a
Nick Phura <Nick.Phura@quartech.com>
2018-06-07 13:50:39 -0700

    bugfix: update unit tests.

--- src/app/pipes/proponent-filter.pipe.spec.ts
+++ src/app/pipes/proponent-filter.pipe.spec.ts
@@ -3,19 +3,20 @@ import { Project } from '../models/project';
 import { Proponent } from '../models/proponent';
 
 describe('ProponentFilterPipe', () => {
-  const q = 'KGHM Ajax Mining Incorporated';
   let value: Project[];
   let expectedResponse: Project[];
 
   const pipe = new ProponentFilterPipe();
 
-  describe('given a valid response', () => {
+  describe('given valid proponents', () => {
     it('returns 1 item', () => {
       value = [new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) })];
       expectedResponse = [new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) })];
 
-      expect(pipe.transform(value, q).length).toBe(expectedResponse.length);
+      const result = pipe.transform(value, 'KGHM Ajax Mining Incorporated');
+      expect(result.length).toBe(expectedResponse.length);
     });
+
     it('returns n items', () => {
       value = [
         new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) }),
@@ -30,32 +31,68 @@ describe('ProponentFilterPipe', () => {
         new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) })
       ];
 
-      expect(pipe.transform(value, q).length).toBe(expectedResponse.length);
+      const result = pipe.transform(value, 'KGHM Ajax Mining Incorporated');
+      expect(result.length).toBe(expectedResponse.length);
+    });
+
+    describe('given a mix of different proponent names', () => {
+      beforeEach(() => {
+        value = [
+          new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) }),
+          new Project({ proponent: new Proponent({ name: 'Under Construction' }) }),
+          new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) }),
+          new Project({ proponent: new Proponent({ name: 'Under Construction' }) })
+        ];
+        expectedResponse = [
+          new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) }),
+          new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) })
+        ];
+      });
+
+      it('returns two items', () => {
+        const result = pipe.transform(value, 'KGHM Ajax Mining Incorporated');
+        expect(result.length).toBe(expectedResponse.length);
+      });
+
+      it('returns Project with same proponent name matches the filer value', () => {
+        const result = pipe.transform(value, 'KGHM Ajax Mining Incorporated');
+        expect(JSON.stringify(result[0].proponent.name)).toBe(JSON.stringify(expectedResponse[0].proponent.name));
+        expect(JSON.stringify(result[1].proponent.name)).toBe(JSON.stringify(expectedResponse[1].proponent.name));
+      });
     });
   });
-  describe('given a mix of different proponent: { names', () => {
+
+  describe('given invalid proponents', () => {
     beforeEach(() => {
       value = [
+        new Project({ proponent: undefined }),
+        new Project({ proponent: new Proponent({ name: null }) }),
         new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) }),
-        new Project({ proponent: new Proponent({ name: 'Under Construction' }) }),
-        new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) }),
-        new Project({ proponent: new Proponent({ name: 'Under Construction' }) })
+        new Project({ proponent: null }),
+        new Project({ proponent: new Proponent({}) }),
+        new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) })
       ];
       expectedResponse = [
         new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) }),
         new Project({ proponent: new Proponent({ name: 'KGHM Ajax Mining Incorporated' }) })
       ];
     });
-    it('returns two items', () => {
-      expect(pipe.transform(value, q).length).toBe(expectedResponse.length);
-    });
-    it('returns Project with same proponent: { name value as value passed in', () => {
-      expect(JSON.stringify(pipe.transform(value, q)[0].proponent.name)).toBe(
-        JSON.stringify(expectedResponse[0].proponent.name)
-      );
-      expect(JSON.stringify(pipe.transform(value, q)[1].proponent.name)).toBe(
-        JSON.stringify(expectedResponse[1].proponent.name)
-      );
+
+    describe('when a proponent or proponent name is null or undefined', () => {
+      let result;
+      beforeEach(() => {
+        result = pipe.transform(value, 'KGHM Ajax Mining Incorporated');
+      });
+
+      it('returns two items', () => {
+        result = pipe.transform(value, 'KGHM Ajax Mining Incorporated');
+        expect(result.length).toBe(expectedResponse.length);
+      });
+
+      it('returns Projects with proponents whose name matches the filter value', () => {
+        expect(JSON.stringify(result[0].proponent.name)).toBe(JSON.stringify(expectedResponse[0].proponent.name));
+        expect(JSON.stringify(result[1].proponent.name)).toBe(JSON.stringify(expectedResponse[1].proponent.name));
+      });
     });
   });
 });
